f8a8843e702e8c67340e2b5e331a84f81695a7a1
cihatdev <cihat02020202@gmail.com>
2020-10-29 18:37:11 +0300

    fix a few mistake

--- calculator/src/components/calculators/other/calculator.vue
+++ calculator/src/components/calculators/other/calculator.vue
@@ -6,7 +6,7 @@
       <div class="buttons">
         <div @click="cleanResult()" class="button">C<b>E</b></div>
         <div @click="cleanAll()" class="button">C</div>
-        <div @click="operate()" class="button">%</div>
+        <div @click="operate('%')" class="button">%</div>
         <div @click="operate('/')" class="button">/</div>
         <div @click="operate(7)" class="button">7</div>
         <div @click="operate(8)" class="button">8</div>
@@ -22,7 +22,7 @@
         <div @click="operate('+')" class="button">+</div>
         <div id="zero" @click="operate(0)" class="button">0</div>
         <div class="button" @click="drop()">
-          <b style="font-weight: 900 font-size: 16px;">⌫</b>
+          <b style="font-weight: 900 font-size: 16px;">←</b>
         </div>
         <div @click="operate('.')" class="button">.</div>
         <div @click="equal()" class="button">=</div>
@@ -44,14 +44,10 @@ export default {
     operate(element) {
       console.log("operate..");
       this.formula += element;
-      // console.log("this.formula:");
-      // console.log(this.formula);
     },
     equal() {
       console.log("equal..");
       this.result = eval(this.formula);
-      // console.log("this.formula:");
-      // console.log(this.formula);
     },
     cleanResult() {
       console.log("cleanResult..");
@@ -65,31 +61,6 @@ export default {
     drop() {
       console.log("drop..");
       this.formula = this.formula.slice(0, -1);
-    },
-    square() {
-      console.log("square..");
-      console.log(eval(this.formula));
-      eval(this.formula) < 0
-        ? (this.formula = "Can not suqre the negative value")
-        : (this.result = Math.sqrt(eval(this.formula)));
-    },
-    devided() {
-      console.log("devided..");
-      this.formula === "" ||
-      this.formula.endsWith("+" || "-" || "*" || "/" || "%")
-        ? {}
-        : (this.formula = "1/(" + this.formula + ")");
-      this.equal();
-    },
-    toggle() {
-      console.log("toggle..");
-      this.formula === "" ||
-      this.formula.endsWith("+" || "-" || "*" || "/" || "%")
-        ? {}
-        : this.formula.startsWith("-")
-        ? (this.formula = Math.abs(eval(this.formula)).toString())
-        : (this.formula = "-(" + this.formula + ")");
-      this.equal();
     },
   },
 };
